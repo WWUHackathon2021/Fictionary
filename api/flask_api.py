3e8454890b08b8eceb84b3f739b6001c29462598
Chris Daw <dawc@wwu.edu>
2021-04-10 23:33:03 -0700

    Link model, website, and API

--- api/flask_api.py
+++ api/flask_api.py
@@ -4,15 +4,34 @@ import pandas as pd
 import json
 import sys
 
+sys.path.append('..')
+import models.train as models
+
 app = Flask(__name__)
 cors = CORS(app)
+
+# ML model definition
+loaded_models = {
+    'plain-1': 'data/pytorch_model.bin',
+    'plain-5': 'data/dict1_epoch5.bin',
+}
+weights = loaded_models['plain-5']
+model = models.get_model_for_api(weights_path=weights)
 
 
 @app.route('/word', methods=["POST"])
 def word():
     data = request.json
-    print(f"user entered {data['word']}")
-    resp_data = json.dumps({'definition': f"{data['word']} but from python"})
+    word = data['word']
+
+    definition = models.define(model, word, num_return=1)[0]
+    definition.replace(']', '').replace('[', '')
+    if definition[-1] != '.':
+        definition += '.'
+
+    resp_data = json.dumps(
+            {'definition': definition}
+        )
     response = Response(resp_data)
     response.headers['Access-Control-Allow-Origin'] = '*'
     response.headers['Access-Control-Allow-Credentials'] = True
@@ -23,5 +42,11 @@ def index():
     return redirect(f'http://hackathon.chrisdaw.net:3030', 301)
 
 if __name__ == '__main__':
+    """
+    # Load ML model
+    for k, weights_path in loaded_models.items():
+        loaded_models[k] = models.get_model(weights_path)
+    """
+
     app.debug=True
     app.run(host='0.0.0.0')  # run our Flask app
