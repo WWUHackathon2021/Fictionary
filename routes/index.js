52bb9ba136142fd7cae732884f91c0dba3b6e8cd
Katie Taylor <katiemxtaylor@gmail.com>
2021-04-10 22:22:51 -0700

    add functionality to get image of text using http module

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
-var express = require('express');
+const express = require('express');
+require('dotenv').config();
 var router = express.Router();
 
 /* GET home page. */
@@ -10,3 +11,44 @@ router.get('/', function (req, res, next) {
 });
 
 module.exports = router;
+
+/* Get image from text */
+const http = require("https");
+
+// let params = "/?text=Logan%20%3D%20A%20kind%20of%20lovable"
+
+const text = encodeURIComponent("Logan = a kind of lovable")
+const color = "000000"
+const backgroundColor = "FFFFFF"
+const fontFamily = "verdana"
+const fontSize = "12"
+const imgType = "png"
+
+const params = "/?text="+text+"&fcolor="+color+"&bcolor="+backgroundColor+"&font="+fontFamily+"&size="+fontSize+"&type="+imgType;
+
+const options = {
+	"method": "GET",
+	"hostname": "img4me.p.rapidapi.com",
+	"port": null,
+	"path": params,
+	"headers": {
+		"x-rapidapi-key": "d541200d34msh8dbf8af02468f86p1ce650jsn3634ce9e5364",
+		"x-rapidapi-host": "img4me.p.rapidapi.com",
+		"useQueryString": true
+	}
+};
+
+const req = http.request(options, function (res) {
+	const chunks = [];
+
+	res.on("data", function (chunk) {
+		chunks.push(chunk);
+	});
+
+	res.on("end", function () {
+		const body = Buffer.concat(chunks);
+		console.log(body.toString());
+	});
+});
+
+req.end();
